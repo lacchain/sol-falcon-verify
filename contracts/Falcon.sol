8edd3fc145686f7c87a26fce435a1264c5d05902
Diego López León <diegol@iadb.org>
2020-10-28 22:47:19 -0300

    Initial commit

---
+++ contracts/Falcon.sol
@@ -0,0 +1,128 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.7.0;
+
+contract Falcon {
+
+  /* ==================================================================== */
+  /*
+   * Error codes.
+   *
+   * Most functions in this API that may fail for some reason return an
+   * 'int' value which will be 0 on success, or a negative error code.
+   * The macros below define the error codes. In the interest of forward
+   * compatibility, callers should be prepared to receive additional error
+   * codes not included in the list below.
+   */
+
+  /*
+   * FALCON_ERR_FORMAT is returned when decoding of an external object
+   * (public key, private key, signature) fails.
+   */
+  int8 constant FALCON_ERR_FORMAT = -3;
+
+  /*
+   * FALCON_ERR_BADSIG is returned when verifying a signature, the signature
+   * is validly encoded, but its value does not match the provided message
+   * and public key.
+   */
+  int8 constant FALCON_ERR_BADSIG = -4;
+
+
+  /*
+   * FALCON_ERR_BADARG is returned when a provided parameter is not in
+   * a valid range.
+   */
+  int8 constant FALCON_ERR_BADARG = -5;
+
+  /* ==================================================================== */
+  /*
+   * Signature formats.
+   */
+
+  /*
+   * Variable-size signature. This format produces the most compact
+   * signatures on average, but the signature size may vary depending
+   * on private key, signed data, and random seed.
+   */
+  uint8 constant FALCON_SIG_COMPRESSED = 1;
+
+  /*
+   * Fixed-size signature. This format produces is equivalent to the
+   * "compressed" format, but includes padding to a known fixed size
+   * (specified by FALCON_SIG_PADDED_SIZE). With this format, the
+   * signature generation loops until an appropriate signature size is
+   * achieved (such looping is uncommon) and adds the padding bytes;
+   * the verification functions check the presence and contents of the
+   * padding bytes.
+   */
+  uint8 constant FALCON_SIG_PADDED = 2;
+
+  /*
+   * Fixed-size format amenable to constant-time implementation. All formats
+   * allow constant-time code with regard to the private key; the 'CT'
+   * format of signature also prevents information about the signature value
+   * and the signed data hash to leak through timing-based side channels
+   * (this feature is rarely needed).
+   */
+  uint8 constant FALCON_SIG_CT = 3;
+
+  function verify(bytes memory signature, uint8 signatureType, bytes memory pubKey, bytes memory data) public pure returns (int8) {
+    int8 r = start(signature);
+    if (r < 0) {
+      return r;
+    }
+    return finish(signature, signatureType, pubKey, keccak256(data));
+  }
+
+  function start(bytes memory signature) private pure returns (int8) {
+    if (signature.length < 41) {
+      return FALCON_ERR_FORMAT;
+    }
+    return 0;
+  }
+
+  function finish(bytes memory signature, uint8 signatureType, bytes memory pubKey, bytes32 dataHash) private pure returns (int8) {
+    if (pubKey.length == 0) {
+      return FALCON_ERR_FORMAT;
+    }
+
+    if ((pubKey[0] & 0xF0) != 0x00) {
+      return FALCON_ERR_FORMAT;
+    }
+
+    uint8 logn = uint8(pubKey[0] & 0x0F);
+    if (logn < 1 || logn > 10) {
+      return FALCON_ERR_FORMAT;
+    }
+
+    if (uint8(signature[0] & 0x0F) != logn) {
+      return FALCON_ERR_BADSIG;
+    }
+
+    bool ct = false;
+    if (signatureType == 0) {
+      byte inferredType = signature[0] & 0xF0;
+      if (inferredType == 0x30 || inferredType == 0x50) {
+        if (signature.length != signatureCtSize(logn)) {
+          return FALCON_ERR_FORMAT;
+        }
+        ct = true;
+      } else {
+        return FALCON_ERR_BADSIG;
+      }
+    } else if (signatureType == FALCON_SIG_COMPRESSED) {
+    } else if (signatureType == FALCON_SIG_PADDED) {
+    } else if (signatureType == FALCON_SIG_CT) {
+    } else {
+      return FALCON_ERR_BADARG;
+    }
+    return 0;
+  }
+
+  /*
+   * Signature size (in bytes) when using the CT format. The size is exact.
+   */
+  function signatureCtSize(uint8 logn) private pure returns (uint16) {
+    return ((uint16(3) << ((logn) - 1)) - ((logn) == 3?1:0) + 41);
+  }
+}
---
+++ test/falcon-512-test.js
@@ -0,0 +1,84 @@
+const Falcon = artifacts.require("Falcon");
+
+contract("Falcon", accounts => {
+  let falcon;
+
+  before(async () => {
+    falcon = await Falcon.deployed();
+  });
+
+  it("has EIP-152 enabled", async () => {
+    // from https://eips.ethereum.org/EIPS/eip-152#test-vector-4
+    let ret = await web3.eth.call({
+      from: accounts[0],
+      to: '0x0000000000000000000000000000000000000009',
+      data: '0x0000000048c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbabd9831f79217e1319cde05b61626300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000001'
+    });
+    assert.equal(ret, '0x08c9bcf367e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d282e6ad7f520e511f6c3e2b8c68059b9442be0454267ce079217e1319cde05b')
+  });
+
+  describe("Public key", async () => {
+    it("cannot be empty", async () => {
+      let pubKey = Buffer.alloc(0);
+      let ret = await falcon.verify.call(Buffer.alloc(42), 0, pubKey, Buffer.alloc(0));
+      assert.equal(ret, -3, 'Error return doesn\'t match Falcon.FALCON_ERR_FORMAT');
+    });
+
+    it("has to begin with 0x0", async() => {
+      let pubKey = Buffer.from('B0', 'hex');
+      let ret = await falcon.verify.call(Buffer.alloc(42), 0, pubKey, Buffer.alloc(0));
+      assert.equal(ret, -3, 'Error return doesn\'t match Falcon.FALCON_ERR_FORMAT');
+    });
+
+    it("has to have second nibble between 1 and 10", async() => {
+      let pubKey = Buffer.from('0B', 'hex');
+      let ret = await falcon.verify.call(Buffer.alloc(42), 0, pubKey, Buffer.alloc(0));
+      assert.equal(ret, -3, 'Error return doesn\'t match Falcon.FALCON_ERR_FORMAT');
+    });
+  })
+
+  describe("Signature", async() => {
+    it("has to be longer than 41 bytes", async () => {
+      let signature = Buffer.alloc(0);
+      let ret = await falcon.verify.call(signature, 0, Buffer.alloc(1), Buffer.alloc(0));
+      assert.equal(ret, -3, 'Error return doesn\'t match Falcon.FALCON_ERR_FORMAT');
+    });
+
+    it("has to match second nibble with public key", async() => {
+      let signature = Buffer.alloc(42);
+      signature.write('0A', 0, 1, 'hex');
+      let pubKey = Buffer.from('09', 'hex');
+      let ret = await falcon.verify.call(signature, 0, pubKey, Buffer.alloc(0));
+      assert.equal(ret, -4, 'Error return doesn\'t match Falcon.FALCON_ERR_BADSIG');
+    });
+  });
+
+  describe("Signature type", async() => {
+    it("has to be 0, 1, 2 or 3", async() => {
+      let signature = Buffer.alloc(42);
+      signature.write('0A', 0, 1, 'hex');
+      let pubKey = Buffer.from('0A', 'hex');
+      let signatureType = 4;
+      let ret = await falcon.verify.call(signature, signatureType, pubKey, Buffer.alloc(0));
+      assert.equal(ret, -5, 'Error return doesn\'t match Falcon.FALCON_ERR_BADARG');
+    });
+
+    it("INFERRED: has to have the first nibble of the signature equal to 3 or 5", async() => {
+      let signature = Buffer.alloc(42);
+      signature.write('0A', 0, 1, 'hex');
+      let pubKey = Buffer.from('0A', 'hex');
+      let signatureType = 0;
+      let ret = await falcon.verify.call(signature, signatureType, pubKey, Buffer.alloc(0));
+      assert.equal(ret, -4, 'Error return doesn\'t match Falcon.FALCON_ERR_BADSIG');
+    })
+
+    it("INFERRED: has to have proper signature length in the public key", async() => {
+      let signature = Buffer.alloc(42);
+      signature.write('31', 0, 1, 'hex');
+      let pubKey = Buffer.from('01', 'hex'); // 1 means signature size of 44 bytes
+      let signatureType = 0;
+      let ret = await falcon.verify.call(signature, signatureType, pubKey, Buffer.alloc(0));
+      assert.equal(ret, -3, 'Error return doesn\'t match Falcon.FALCON_ERR_FORMAT');
+    });
+  });
+});
