b364d86703dc4be47683fe4980454b22a351f597
Diego López León <diegol@iadb.org>
2020-11-02 15:46:00 -0300

    Send explicit transaction on every call so gas reporter tracks consumption

--- test/falcon-512-test.js
+++ test/falcon-512-test.js
@@ -20,20 +20,29 @@ contract("Falcon", accounts => {
   describe("Public key", async () => {
     it("cannot be empty", async () => {
       let pubKey = Buffer.alloc(0);
-      let ret = await falcon.verify.call(Buffer.alloc(42), 0, pubKey, Buffer.alloc(0));
+      let verifyArgs = [Buffer.alloc(42), 0, pubKey, Buffer.alloc(0)];
+      let ret = await falcon.verify.call.apply(null, verifyArgs);
       assert.equal(ret, -3, 'Error return doesn\'t match Falcon.FALCON_ERR_FORMAT');
+      let tx = await falcon.verify.sendTransaction.apply(null, verifyArgs);
+      assert.equal(tx.receipt.status, true);
     });
 
     it("has to begin with 0x0", async() => {
       let pubKey = Buffer.from('B0', 'hex');
-      let ret = await falcon.verify.call(Buffer.alloc(42), 0, pubKey, Buffer.alloc(0));
+      let verifyArgs = [Buffer.alloc(42), 0, pubKey, Buffer.alloc(0)];
+      let ret = await falcon.verify.call.apply(null, verifyArgs);
       assert.equal(ret, -3, 'Error return doesn\'t match Falcon.FALCON_ERR_FORMAT');
+      let tx = await falcon.verify.sendTransaction.apply(null, verifyArgs);
+      assert.equal(tx.receipt.status, true);
     });
 
     it("has to have second nibble between 1 and 10", async() => {
       let pubKey = Buffer.from('0B', 'hex');
-      let ret = await falcon.verify.call(Buffer.alloc(42), 0, pubKey, Buffer.alloc(0));
+      let verifyArgs = [Buffer.alloc(42), 0, pubKey, Buffer.alloc(0)];
+      let ret = await falcon.verify.call.apply(null, verifyArgs);
       assert.equal(ret, -3, 'Error return doesn\'t match Falcon.FALCON_ERR_FORMAT');
+      let tx = await falcon.verify.sendTransaction.apply(null, verifyArgs);
+      assert.equal(tx.receipt.status, true);
     });
 
     it("has to have proper length", async() => {
@@ -42,24 +51,33 @@ contract("Falcon", accounts => {
       let pubKey = Buffer.alloc(4);
       pubKey.write('01', 0, 1, 'hex'); // 1 means pub key size of 5 bytes
       let signatureType = 1;
-      let ret = await falcon.verify.call(signature, signatureType, pubKey, Buffer.alloc(0));
+      let verifyArgs = [signature, signatureType, pubKey, Buffer.alloc(0)];
+      let ret = await falcon.verify.call.apply(null, verifyArgs);
       assert.equal(ret, -3, 'Error return doesn\'t match Falcon.FALCON_ERR_FORMAT');
+      let tx = await falcon.verify.sendTransaction.apply(null, verifyArgs);
+      assert.equal(tx.receipt.status, true);
     });
   })
 
   describe("Signature", async() => {
     it("has to be longer than 41 bytes", async () => {
       let signature = Buffer.alloc(0);
-      let ret = await falcon.verify.call(signature, 0, Buffer.alloc(1), Buffer.alloc(0));
+      let verifyArgs = [signature, 0, Buffer.alloc(1), Buffer.alloc(0)];
+      let ret = await falcon.verify.call.apply(null, verifyArgs);
       assert.equal(ret, -3, 'Error return doesn\'t match Falcon.FALCON_ERR_FORMAT');
+      let tx = await falcon.verify.sendTransaction.apply(null, verifyArgs);
+      assert.equal(tx.receipt.status, true);
     });
 
     it("has to match second nibble with public key", async() => {
       let signature = Buffer.alloc(42);
       signature.write('0A', 0, 1, 'hex');
       let pubKey = Buffer.from('09', 'hex');
-      let ret = await falcon.verify.call(signature, 0, pubKey, Buffer.alloc(0));
+      let verifyArgs = [signature, 0, pubKey, Buffer.alloc(0)];
+      let ret = await falcon.verify.call.apply(null, verifyArgs);
       assert.equal(ret, -4, 'Error return doesn\'t match Falcon.FALCON_ERR_BADSIG');
+      let tx = await falcon.verify.sendTransaction.apply(null, verifyArgs);
+      assert.equal(tx.receipt.status, true);
     });
   });
 
@@ -69,8 +87,11 @@ contract("Falcon", accounts => {
       signature.write('0A', 0, 1, 'hex');
       let pubKey = Buffer.from('0A', 'hex');
       let signatureType = 4;
-      let ret = await falcon.verify.call(signature, signatureType, pubKey, Buffer.alloc(0));
+      let verifyArgs = [signature, signatureType, pubKey, Buffer.alloc(0)];
+      let ret = await falcon.verify.call.apply(null, verifyArgs);
       assert.equal(ret, -5, 'Error return doesn\'t match Falcon.FALCON_ERR_BADARG');
+      let tx = await falcon.verify.sendTransaction.apply(null, verifyArgs);
+      assert.equal(tx.receipt.status, true);
     });
 
     it("INFERRED: has to have the first nibble of the signature equal to 3 or 5", async() => {
@@ -78,17 +99,23 @@ contract("Falcon", accounts => {
       signature.write('0A', 0, 1, 'hex');
       let pubKey = Buffer.from('0A', 'hex');
       let signatureType = 0;
-      let ret = await falcon.verify.call(signature, signatureType, pubKey, Buffer.alloc(0));
+      let verifyArgs = [signature, signatureType, pubKey, Buffer.alloc(0)];
+      let ret = await falcon.verify.call.apply(null, verifyArgs);
       assert.equal(ret, -4, 'Error return doesn\'t match Falcon.FALCON_ERR_BADSIG');
-    })
+      let tx = await falcon.verify.sendTransaction.apply(null, verifyArgs);
+      assert.equal(tx.receipt.status, true);
+    });
 
     it("INFERRED: has to have proper signature length in the public key", async() => {
       let signature = Buffer.alloc(42);
       signature.write('31', 0, 1, 'hex');
       let pubKey = Buffer.from('01', 'hex'); // 1 means signature size of 44 bytes
       let signatureType = 0;
-      let ret = await falcon.verify.call(signature, signatureType, pubKey, Buffer.alloc(0));
+      let verifyArgs = [signature, signatureType, pubKey, Buffer.alloc(0)];
+      let ret = await falcon.verify.call.apply(null, verifyArgs);
       assert.equal(ret, -3, 'Error return doesn\'t match Falcon.FALCON_ERR_FORMAT');
+      let tx = await falcon.verify.sendTransaction.apply(null, verifyArgs);
+      assert.equal(tx.receipt.status, true);
     });
 
     it("COMPRESSED: has to have first nibble of signature equals 3", async() => {
@@ -96,8 +123,11 @@ contract("Falcon", accounts => {
       signature.write('21', 0, 1, 'hex');
       let pubKey = Buffer.from('01', 'hex');
       let signatureType = 1;
-      let ret = await falcon.verify.call(signature, signatureType, pubKey, Buffer.alloc(0));
+      let verifyArgs = [signature, signatureType, pubKey, Buffer.alloc(0)];
+      let ret = await falcon.verify.call.apply(null, verifyArgs);
       assert.equal(ret, -3, 'Error return doesn\'t match Falcon.FALCON_ERR_FORMAT');
+      let tx = await falcon.verify.sendTransaction.apply(null, verifyArgs);
+      assert.equal(tx.receipt.status, true);
     });
 
     it("PADDED: has to have first nibble of signature equals 3", async() => {
@@ -105,8 +135,11 @@ contract("Falcon", accounts => {
       signature.write('21', 0, 1, 'hex');
       let pubKey = Buffer.from('01', 'hex');
       let signatureType = 2;
-      let ret = await falcon.verify.call(signature, signatureType, pubKey, Buffer.alloc(0));
+      let verifyArgs = [signature, signatureType, pubKey, Buffer.alloc(0)];
+      let ret = await falcon.verify.call.apply(null, verifyArgs);
       assert.equal(ret, -3, 'Error return doesn\'t match Falcon.FALCON_ERR_FORMAT');
+      let tx = await falcon.verify.sendTransaction.apply(null, verifyArgs);
+      assert.equal(tx.receipt.status, true);
     });
 
     it("PADDED: has to have proper signature length in the public key", async() => {
@@ -114,8 +147,11 @@ contract("Falcon", accounts => {
       signature.write('31', 0, 1, 'hex');
       let pubKey = Buffer.from('01', 'hex'); // 1 means signature size of 44 bytes
       let signatureType = 2;
-      let ret = await falcon.verify.call(signature, signatureType, pubKey, Buffer.alloc(0));
+      let verifyArgs = [signature, signatureType, pubKey, Buffer.alloc(0)];
+      let ret = await falcon.verify.call.apply(null, verifyArgs);
       assert.equal(ret, -3, 'Error return doesn\'t match Falcon.FALCON_ERR_FORMAT');
+      let tx = await falcon.verify.sendTransaction.apply(null, verifyArgs);
+      assert.equal(tx.receipt.status, true);
     });
 
     it("CONSTANT-TIME: has to have first nibble of signature equals 5", async() => {
@@ -123,8 +159,11 @@ contract("Falcon", accounts => {
       signature.write('41', 0, 1, 'hex');
       let pubKey = Buffer.from('01', 'hex'); // 1 means signature size of 44 bytes
       let signatureType = 3;
-      let ret = await falcon.verify.call(signature, signatureType, pubKey, Buffer.alloc(0));
+      let verifyArgs = [signature, signatureType, pubKey, Buffer.alloc(0)];
+      let ret = await falcon.verify.call.apply(null, verifyArgs);
       assert.equal(ret, -3, 'Error return doesn\'t match Falcon.FALCON_ERR_FORMAT');
+      let tx = await falcon.verify.sendTransaction.apply(null, verifyArgs);
+      assert.equal(tx.receipt.status, true);
     });
 
     it("CONSTANT-TIME: has to have proper signature length in the public key", async() => {
@@ -132,8 +171,11 @@ contract("Falcon", accounts => {
       signature.write('51', 0, 1, 'hex');
       let pubKey = Buffer.from('01', 'hex'); // 1 means signature size of 44 bytes
       let signatureType = 3;
-      let ret = await falcon.verify.call(signature, signatureType, pubKey, Buffer.alloc(0));
+      let verifyArgs = [signature, signatureType, pubKey, Buffer.alloc(0)];
+      let ret = await falcon.verify.call.apply(null, verifyArgs);
       assert.equal(ret, -3, 'Error return doesn\'t match Falcon.FALCON_ERR_FORMAT');
+      let tx = await falcon.verify.sendTransaction.apply(null, verifyArgs);
+      assert.equal(tx.receipt.status, true);
     });
   });
 });
