fce8b56b21a0d8a04b2dffaa322ecf8d0373b8cd
Diego López León <diegol@iadb.org>
2021-02-24 16:39:33 -0300

    Parse NIST signatures

--- test/falcon512-KAT.js
+++ test/falcon512-KAT.js
@@ -12,8 +12,19 @@ contract("Falcon", accounts => {
         const falcon = await Falcon.deployed();
 
         kats.slice(0, 3).forEach(kat => {
-          suite.addTest(new Test(`${kat.count}`, async() => {
-            const contractReturn = await falcon.verify.call(0, Buffer.from(kat.sm, 'hex').toJSON().data, kat.smlen, Buffer.from(kat.msg, 'hex').toJSON().data, kat.mlen, Buffer.from(kat.pk, 'hex').toJSON().data, 897);
+          suite.addTest(new Test(`KAT test ${kat.count}`, async() => {
+            const nistSignatureBuffer = Buffer.from(kat.sm, 'hex');
+
+            const messageLength = parseInt(kat.mlen);
+
+            const signatureBuffer = nistSignatureBuffer.slice(2 + 40 + messageLength); // skip 2 bytes for signature size + 40 bytes for nonce + message length
+            const signatureLength = nistSignatureBuffer.readInt16BE(0);
+            const messageBuffer = Buffer.from(kat.msg, 'hex');
+            const publicKeyBuffer = Buffer.from(kat.pk, 'hex');
+
+            //console.log('signature[%d]: %s\nmessage[%d]: %s\npk[897]: %s', signatureLength, signatureBuffer.toString('hex'), messageLength, messageBuffer.toString('hex'), publicKeyBuffer.toString('hex'));
+
+            const contractReturn = await falcon.verify.call(0, Array.from(signatureBuffer), signatureLength, Array.from(messageBuffer), messageLength, Array.from(publicKeyBuffer), 897);
             assert.equal(getFalconReturnValue(contractReturn), 0, `Reason: ${getReasonCode(contractReturn)}`);
           }));
         });
